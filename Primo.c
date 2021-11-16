4b0a734bd67dfa77f67dc7b87e54ad9283973966
GiovanniPiferi <94382806+GiovanniPiferi@users.noreply.github.com>
2021-11-16 14:42:53 +0100

    Update Primo.c

--- Primo.c
+++ Primo.c
@@ -7,12 +7,12 @@ int  main ( int argc, char *argv[]) {
   if (argc> 1 ) printf ( " Primo argomento utente: %s\n " , argv[ 1 ]);
   if (argc> 2 ){
     anni = atoi (argv[ 2 ]);
-    se (anni != 0 )
-      printf ( " Secondo argomento utente ( %s ) valido %d \n " , argv[ 2 ], anni);
-    altro {
-      printf ( " secondo argomento utente ( %s ) non valido \n " argv[ 2 ]);
-      ritorno - 1 ;
+    if (anni != 0 )
+        printf ("Secondo argomento utente ( %s ) valido %d\n" , argv[ 2 ], anni);
+    else {
+        printf ("secondo argomento utente ( %s ) non valido\n" argv[ 2 ]);
+        return -1;
     }
   }
-  ritorno  0 ;
+  return  0;
 }
