fa5bb971220094c41680efb16217636f330000e0
Giovanni Piferi <94382806+GiovanniPiferi@users.noreply.github.com>
2021-11-16 18:54:47 +0100

    Update Secondo.c

--- Secondo.c
+++ Secondo.c
@@ -4,9 +4,20 @@
 #include "elabora.h"
 
 /*
-void elabora(int contatore_parametri, char "paramentri[]) {
+void elabora(int contatore_parametri, char *paramentri[]) {
     char buffer[4];
     strncpy(buffer, parametri[1], sizeof(buffer));
     printf("%s/n", buffer);
 }
 */
+
+int main(int argc, char *argv[]) {
+    int risultato;
+    char "ptrNomeProgramma = malloc(10);
+    char "ptrPrimoArgomento = Malloc(4);
+    char "array_parametri[2];
+    array_parametri[0] = ptrNomeProgramma;
+    array_parametri[1] = ptrPrimoArgomento;
+    elabora(2, array_Parametri);
+    return 0;
+}
