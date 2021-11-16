3afd88f3e12eb460a54959f0e030be39587d5310
Giovanni Piferi <94382806+GiovanniPiferi@users.noreply.github.com>
2021-11-16 18:47:10 +0100

    Update Secondo.c

--- Secondo.c
+++ Secondo.c
@@ -2,3 +2,11 @@
 #include  < stdlib.h >
 
 #include "elabora.h"
+
+/*
+void elabora(int contatore_parametri, char "paramentri[]) {
+    char buffer[4];
+    strncpy(buffer, parametri[1], sizeof(buffer));
+    printf("%s/n", buffer);
+}
+*/
